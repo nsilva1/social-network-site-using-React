82e8d71cfda1313ea130b3e8281e0d8de416f4ca
Nascimento Silva <ukpongneto@gmail.com>
2018-08-14 14:10:05 +0100

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://neto:netoneto9@ds255309.mlab.com:55309/devnet",
+  mongoURL: "mongodb://localhost:27017/devsocialnetwork"
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/userAuth");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+var app = express();
+var port = process.env.PORT || 8078;
+
+//DB config
+const db = require("./config/keys");
+
+//connect to MongoDB
+mongoose
+  .connect(db.mongoURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+//set view engine to handlebars. looks in views folder for html template to render
+app.set("view engine", "hbs");
+
+//get URL string and send a response everytime that string appears.
+app.get("/", (req, res) => {
+  //res.send("");
+  res.render("page.hbs", {
+    pageTitle: "Wedding Page"
+  });
+});
+
+//use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
