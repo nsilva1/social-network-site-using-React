dfc137a5e31ecc44fb27a80e3bd128f223b8e880
Nascimento Silva <ukpongneto@gmail.com>
2019-02-01 14:55:39 +0100

    Decode token payload and set current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import jwt_decode from 'jwt-decode';
+
+// Import Function to apply token to every request from logged in user
+import setAuthToken from '../utils/setAuthToken';
 
 //Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -12,4 +16,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      //Save Token in Local Storage
+      const { token } = res.data;
+      //Set Token to Local Storage
+      localStorage.setItem('jwtToken', token);
+      //Set Token to auth Header
+      setAuthToken(token);
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      //Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Set Logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
