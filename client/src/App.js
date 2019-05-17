6bbdb581b20c584b34e0edf27df22a1d7ec3e874
Nascimento Silva <ukpongneto@gmail.com>
2019-05-17 11:32:06 +0100

    Edit Profile

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
 
 //Import Login and Registration
 import Login from './components/auth/login';
@@ -62,6 +63,13 @@ class App extends Component {
                   exact
                   path="/create-profile"
                   component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
                 />
               </Switch>
             </div>
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 import {
   GET_PROFILE,
   PROFILE_LOADING,
-  // GET_ERRORS,
+  GET_ERRORS,
   CLEAR_CURRENT_PROFILE,
-  GET_ERRORS
+  GET_ERRORS,
+  SET_CURRENT_USER
 } from './types';
 
 //Get Current Profile
@@ -52,4 +53,24 @@ export const clearCurrentProfile = () => {
   return {
     type: CLEAR_CURRENT_PROFILE
   };
+};
+
+//Delete Account & Profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm('Are you sure? This action cannot be undone')) {
+    axios
+      .delete('/api/profile')
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 };
